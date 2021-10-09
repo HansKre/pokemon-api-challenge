a2e7f617f6408d5fae4694ee19c6904fefdcc7b9
hans <hans.krebs@me.com>
2021-10-09 11:41:59 +0200

    Add optional margins and remove vertical center.

--- components/column/Column.tsx
+++ components/column/Column.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
-export const Column = styled.div`
+const Column = styled.div<{ margin?: string }>`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
-  justify-content: center;
   align-items: center;
+  margin: ${({ margin }) => margin};
 `;
+
+export default Column;
