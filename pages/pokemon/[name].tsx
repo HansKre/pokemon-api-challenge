7d74f0127a6c1edfb397b5c9ea91d6dbb5308163
hans <hans.krebs@me.com>
2021-10-09 12:30:03 +0200

    Properly implement details page.

--- pages/pokemon/[name].tsx
+++ pages/pokemon/[name].tsx
@@ -3,23 +3,34 @@ import { ComponentProps } from '../_app';
 import { PokemonI } from '../../types';
 import { useEffect, useState } from 'react';
 import { POKEMON_API } from '../../constants';
+import Column from '../../components/column/Column';
+import WithFadeInAnimation from '../../components/animations/WithFadeInAnimation';
+import { Button } from '../../components/button/Button';
+import PokemonDetails from '../../components/pokemon-card/PokemonDetails';
+import Loader from '../../components/loader/Loader';
 
 export default function Pokemon({ pokemons }: ComponentProps) {
   const router = useRouter();
   const name = router.query.name as string;
   const [pokemon, setPokemon] = useState<PokemonI>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  // TODO: add error handling
+  // TODO: add loading animation when no data
   useEffect(() => {
     async function doFetch() {
+      setIsLoading(true);
       const pokemon: PokemonI = await fetch(`${POKEMON_API}${name}`)
         .then((res) => res.json())
         .catch((reason) => {
           console.log(reason);
-          // setIsError(true);
+          setIsError(true);
         });
-      // setIsError(false);
-      setPokemon(pokemon);
+      setIsError(false);
+      if (pokemon) {
+        setPokemon(pokemon);
+      }
+      setIsLoading(false);
     }
     if (pokemons) {
       const result = pokemons?.find((pokemon) => pokemon.name === name);
@@ -32,10 +43,16 @@ export default function Pokemon({ pokemons }: ComponentProps) {
   }, [name, pokemons]);
 
   return (
-    <>
-      <button onClick={() => router.push('/')}>Go back</button>
-      <h1>{name}</h1>
-      {pokemon && <h2>weight: {pokemon.weight}</h2>}
-    </>
+    <WithFadeInAnimation>
+      <Column margin='0 20vw 0 20vw'>
+        <Button onClick={() => router.push('/')}>
+          &larr; Back to all Pokemons
+        </Button>
+        {isError && <p>API Request failed</p>}
+        {pokemon && <PokemonDetails pokemon={pokemon} />}
+        {!pokemon && !isLoading && <p>Not a Pokemin -.-</p>}
+        {isLoading && <Loader />}
+      </Column>
+    </WithFadeInAnimation>
   );
 }
