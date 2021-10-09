af2b753bb07148759bb8e75e426fef06f2ade452
hans <hans.krebs@me.com>
2021-10-09 10:13:41 +0200

    Let animation behave neutral in layout.

--- components/animations/WithFadeInAnimation.tsx
+++ components/animations/WithFadeInAnimation.tsx
@@ -1,18 +1,24 @@
 import { ReactNode } from 'react';
 import { motion } from 'framer-motion';
+import styled from 'styled-components';
 
 type Props = {
   children: ReactNode;
 };
 
+const MotionDiv = styled(motion.div)`
+  display: flex;
+  flex: auto;
+`;
+
 export default function WithFadeInAnimation({ children }: Props) {
   return (
-    <motion.div
+    <MotionDiv
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
       {children}
-    </motion.div>
+    </MotionDiv>
   );
 }
