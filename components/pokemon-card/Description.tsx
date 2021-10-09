ebe382e722f01b10bf46d29b32df9f2e47790bf3
hans <hans.krebs@me.com>
2021-10-09 11:41:30 +0200

    Add bold-attribute to Description.

--- components/pokemon-card/Description.tsx
+++ components/pokemon-card/Description.tsx
@@ -1,10 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Description = styled.p`
+const Description = styled.p<{ bold?: boolean }>`
   margin: 0;
   font-size: ${(props) => props.theme.typography.p};
   line-height: 1.5;
-  font-weight: 100;
+  font-weight: ${({ bold }) => (bold ? 700 : 100)};
+  ${({ bold }) =>
+    bold &&
+    css`
+      padding-top: 0.8rem;
+    `}
 `;
 
 export default Description;
